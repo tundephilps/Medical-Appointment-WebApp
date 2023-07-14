5c9c186a777c1054ea3f0df1d77466bdafb8687d
Tundephilps <tundephilps@gmail.com>
2023-07-14 15:42:28 +0100

    sixth commit

--- components/Contact/UpSide.jsx
+++ components/Contact/UpSide.jsx
@@ -21,7 +21,7 @@ const UpSide = () => {
 
       <div className="px-2">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448181.163741503!2d76.81306412566978!3d28.647279935598277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1668254278259!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31717.758664833418!2d3.40228779627649!3d6.430026633978033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf52dc0328169%3A0x7885a1a33689a576!2sVictoria%20Island%20106104%2C%20Lagos!5e0!3m2!1sen!2sng!4v1689345402919!5m2!1sen!2sng"
           width={800}
           height={600}
           style={{ border: 0 }}
